164ca1efb132a917ba81fd8f3354127584bdf068
Mario Napolano <marionapolano@gmail.com>
2023-01-09 19:01:25 +0100

    no message

--- FE/chatbot/src/App.js
+++ FE/chatbot/src/App.js
@@ -3,14 +3,21 @@ import './App.css';
 import './style.css';
 import img from './assets/send.svg'
 import {handleSubmit} from './script.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
+  var userLang = navigator.language || navigator.userLanguage; 
   return (
-    <div >
-      <div id="chat_container"></div>
-      <form>
-        <textarea name="prompt" rows="1" cols="1" placeholder="Chiedi"></textarea>
+    <div className='imageBackGround' >
+    <div className='container '  >
+      <div id="chat_container"  ></div>
+      <div  className="textBottom ">
+      <form className="mb-2 " >
+        <textarea name="prompt" className='textarea' rows="1" cols="1" placeholder={userLang=="it"?"Chiedi...":"Ask..."}></textarea>
         <button onClick={(e)=>handleSubmit(e)}><img src={img} alt="send" /></button>
       </form>
+      </div>
+    </div>
     </div>
   );
 }
